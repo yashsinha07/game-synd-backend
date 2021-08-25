897d2a1d21144de9f7f105f1619db5a10d6c02b4
yashsinha07 <yashsinha7696@gmail.com>
2021-08-25 12:08:22 +0530

    adding Spring Application class

---
+++ src/main/java/com/inmobi/gamesyndbackend/GameSyndBackendApplication.java
@@ -0,0 +1,13 @@
+package com.inmobi.gamesyndbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameSyndBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameSyndBackendApplication.class, args);
+    }
+
+}
